da1a86a2e035a39698442898891ae4ac7b3492ee
Ramon Santos <rsanttos22@gmail.com>
2018-09-22 21:18:17 -0300

    Melhorias no cadastro de tarefas

--- js/rs_todo_list.js
+++ js/rs_todo_list.js
@@ -2,12 +2,20 @@ var divTasksToDo = document.getElementById("tasks_to_do");
 var divTasksDoing = document.getElementById("tasks_doing");
 var divTasksDone = document.getElementById("tasks_done");
 
+var inputTaskTitle = document.getElementById("task_title");
+
 var classSubCard = "card-body";
 
 function addTask(){
-    var newDiv = document.createElement("div"); 
-    newDiv.class = classSubCard;
-    divTasksToDo.append(newDiv);
+    var valueInput = inputTaskTitle.value;
+    if(valueInput != ""){
+        var newDiv = document.createElement("div"); 
+        newDiv.className = classSubCard;
+        newDiv.innerHTML += valueInput;
+        divTasksToDo.append(newDiv);
+        valueInput = "";
+        inputTaskTitle.value = valueInput;
+    }
 }
 
 function moveTaskToDoing(){
